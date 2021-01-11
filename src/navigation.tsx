08d1c17c024e6c77a35c22ec5ec9fed0c670d167
YuyaAbo <b1011211@gmail.com>
2021-01-12 01:19:59 +0900

    date navigation(prev, next) by React state hook
    ref https://ja.reactjs.org/docs/hooks-intro.html
    ref https://gotohayato.com/content/509/

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,22 +1,49 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { CalendarToday } from '@material-ui/icons';
 import { Button, IconButton } from "@material-ui/core";
 
+const Items = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding-top: 1rem;
+`
+
 function Navigation() {
-    const Items = styled.div`
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding-top: 1rem;
-    `
+    const [currentDate, setCurrentDate] = useState(new Date())
+
+    const prevMonth = () => {
+        let d = new Date()
+        if (currentDate.getMonth() === 0) {
+            d.setFullYear(currentDate.getFullYear() - 1)
+            d.setMonth(11)
+        } else {
+            d.setFullYear(currentDate.getFullYear())
+            d.setMonth(currentDate.getMonth() - 1)
+        }
+        return d
+    }
+
+    const nextMonth = () => {
+        let d = new Date()
+        if (currentDate.getMonth() === 11) {
+            d.setFullYear(currentDate.getFullYear() + 1)
+            d.setMonth(0)
+        } else {
+            d.setFullYear(currentDate.getFullYear())
+            d.setMonth(currentDate.getMonth() + 1)
+        }
+        return d
+    }
 
     return (
         <Items>
             <CalendarToday />
             <p>カレンダー</p>
-            <IconButton>＜</IconButton>
-            <IconButton>＞</IconButton>
-            <Button>2020年1月</Button>
+            <IconButton onClick={() => setCurrentDate(prevMonth())}>＜</IconButton>
+            <IconButton onClick={() => setCurrentDate(nextMonth())}>＞</IconButton>
+            <Button>{currentDate.getFullYear()} 年 {currentDate.getMonth() + 1} 月</Button>
         </Items>
     )
 }
